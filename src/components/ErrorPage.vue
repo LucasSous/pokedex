b9c0750b8fd0d482529c5eac652e9f6cc0b74539
LucaSous <lucassousadasilva333@gmail.com>
2022-10-09 22:33:01 -0300

    feat: error page

---
+++ src/components/ErrorPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-row>
+    <v-col cols="12" lg="6" class="d-flex justify-end align-center image">
+      <img src="../assets/pikachu.png" width="70%" alt="sad-pikachu" />
+    </v-col>
+    <v-col cols="12" lg="6" class="d-flex align-center message">
+      <h1>Algo deu errado...</h1>
+    </v-col>
+  </v-row>
+</template>
+
+<style>
+@media (max-width: 768px) {
+  .message {
+    justify-content: center;
+  }
+}
+</style>
--- src/views/home/HomeView.vue
+++ src/views/home/HomeView.vue
@@ -1,14 +1,20 @@
 <template>
-  <div class="home-view">
-    <NavBar v-model="search" @input="filter" />
-    <v-row rows="12" v-if="pokemons.length">
-      <v-col cols="12" lg="2" md="4" sm="6" v-for="pokemon in pokemons" :key="pokemon.name">
-        <Cards :name="pokemon.data.name" :pokemonImg="pokemon.data.sprites.front_default" />
-      </v-col>
-    </v-row>
-    <Loading v-if="isLoading" />
-    <div v-if="search && !pokemons.length" class="d-flex justify-center">
-      <h3>Nenum Pokemon encontrado!</h3>
+  <div class="home-view d-flex flex-column justify-space-between">
+    <div>
+      <NavBar v-model="search" @input="filter" />
+      <v-row rows="12" v-if="pokemons.length">
+        <v-col cols="12" lg="2" md="4" sm="6" v-for="pokemon in pokemons" :key="pokemon.name">
+          <Cards :name="pokemon.data.name" :pokemonImg="pokemon.data.sprites.front_default" />
+        </v-col>
+      </v-row>
+      <Loading v-if="isLoading" />
+      <div v-if="search && !pokemons.length" class="d-flex justify-center">
+        <h3>Nenum Pokemon encontrado!</h3>
+      </div>
+      <ErrorPage v-if="isError" />
+    </div>
+    <div class="rodape d-flex justify-center pa-6 mt-10">
+      <a href="https://github.com/LucasSous" target="blanck">@Lucas Sousa</a>
     </div>
   </div>
 </template>
@@ -18,28 +24,41 @@ import NavBar from '@/components/NavBar.vue';
 import Cards from './components/Cards.vue';
 import axios from 'axios';
 import Loading from '@/components/Loading.vue';
+import ErrorPage from '@/components/ErrorPage.vue';
 
 export default {
   name: 'Home',
-  components: { NavBar, Cards, Loading },
+  components: { NavBar, Cards, Loading, ErrorPage },
   data: () => ({
     pokemons: [],
     initalValues: [],
     isLoading: false,
-    search: ''
+    search: '',
+    isError: false
   }),
+
+  mounted() {
+    this.getPokemons();
+  },
+
   methods: {
     getPokemons() {
       this.isLoading = true;
       let endpoints = [];
-      for (let i = 1; i < 300; i++) {
+      for (let i = 1; i < 500; i++) {
         endpoints.push(`https://pokeapi.co/api/v2/pokemon/${i}/`);
       }
-      axios.all(endpoints.map((endpoint) => axios.get(endpoint))).then((response) => {
-        this.pokemons = response;
-        this.initalValues = response;
-        this.isLoading = false;
-      });
+      axios
+        .all(endpoints.map((endpoint) => axios.get(endpoint)))
+        .then((response) => {
+          this.pokemons = response;
+          this.initalValues = response;
+          this.isLoading = false;
+        })
+        .catch(() => {
+          this.isError = true;
+          this.isLoading = false;
+        });
     },
 
     filter() {
@@ -52,15 +71,18 @@ export default {
       );
       this.pokemons = filter;
     }
-  },
-  mounted() {
-    this.getPokemons();
   }
 };
 </script>
 
 <style scoped>
 .home-view {
+  height: 100%;
   padding: 15px;
+}
+
+.rodape a {
+  text-decoration: none;
+  color: #1c1c1c;
 }
 </style>
